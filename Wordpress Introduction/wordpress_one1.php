6e7e537f890224bce6dc56d3881db0716e4fe732
unknown <sanjoykundu187@gmail.com>
2024-05-13 01:16:30 +0600

    all extension completed

--- Wordpress Introduction/wordpress_one1.php
+++ Wordpress Introduction/wordpress_one1.php
@@ -1,4 +1,45 @@
 <?php 
+/**
+ * Step:1 
+   * wordpress er jonno chrome browser open korbo.akta browser open kore nibo wordpress name
+   * Tools
+     * a. lightshort screen short
+     * b.SpyBar (website ki diye build kora seita dekbo)
+     *   url: https://chrome.google.com/webstore/detail/spybar/dkcihmjnfimlnmdjoddhjfiihbfpcnfk/related?utm_source=www.crx4chrome.com
+     
+     *c. akta website ki diye toyri seta dekhar jonno amra what cms like check korte pari url: https://whatcms.org/
+
+    *d.color er jonno colorZila extension use korbo 
+       *url: https://www.colorzilla.com/chrome/welcome/new/?chrome/124.0.0.0/-/4.0
+    *e. color picker use korte pari: 
+    
+    *f.css peeper chrome extension: url: https://chromewebstore.google.com/detail/css-peeper/mbnbehikldjhnfehhnaidhjhoofhpehk
+    wesite e joto color and image use kora hoyece sob dekte pabo
+
+
+    *g. oneline e note korar jonno (notepad.pw) likhe note korte parbo tps://notepad.pw/lWolZkAVm5SkJKwqMNGe
+    
+    *h. linked notes. url:https://chromewebstore.google.com/detail/linkednotes/kmmbfbgimpbcaaeibgnijiflciagmiha
+
+
+    *i. upwork er jonno aita lagbe go full page screen capture 
+    *url: https://chromewebstore.google.com/detail/linkednotes/kmmbfbgimpbcaaeibgnijiflciagmiha
+    
+    *j. amra page ruller extension use korte pari. url: https://chromewebstore.google.com/detail/linkednotes/kmmbfbgimpbcaaeibgnijiflciagmiha
+
+
+    *k. font er jonno font finder use korte pari
+    * url: https://chromewebstore.google.com/detail/linkednotes/kmmbfbgimpbcaaeibgnijiflciagmiha
+
+
+    *l. Image downloaded extension. url:https://chromewebstore.google.com/detail/linkednotes/kmmbfbgimpbcaaeibgnijiflciagmiha
+
+
+
+    *m. file transfer er jonno wetransfer.com user korbo google driver er poriborte
+
+    *n. 
+ */
 
 
 
